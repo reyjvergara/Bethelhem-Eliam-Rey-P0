e0501c2a923390f8a50d8cc315e872ac77a2a87b
reyjvergara <rey.j.vergara@gmail.com>
2023-05-23 13:02:38 -0700

    gitflow

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,5 +3,6 @@ package com.mycompany.app;
 public class App {
   public static void main(String args[]) {
     System.out.println("Welcome to eCommerce App!");
+    System.out.println("GitFlow");
   }
 }
