0b33e6598f3d0f52fba901367ab32b1434eebcb5
reyjvergara <rey.j.vergara@gmail.com>
2023-05-25 10:59:27 -0700

    added router service to main app

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,12 @@
 package com.mycompany.app;
 
+import java.util.Scanner;
+
+import com.mycompany.app.services.RouterService;
+
 public class App {
   public static void main(String args[]) {
     System.out.println("Welcome to eCommerce App!");
+    RouterService router = new RouterService();
   }
 }
