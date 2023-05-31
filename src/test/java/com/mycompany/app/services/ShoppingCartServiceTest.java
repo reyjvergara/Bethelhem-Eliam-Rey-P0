45c0125cfdf222fb088ff71745d55818f3ec64c7
reyjvergara <80667044+reyjvergara@users.noreply.github.com>
2023-05-31 06:02:36 -0700

    Productservicetests (#43)
    * gitflow
    
    * Added SQL related files
    
    * Updated SQL related parts and .gitignore
    
    * Delete target/classes directory
    
    * Update App.java
    
    * added empty test cases to iterate upon later
    
    * Google Java Format
    
    ---------
    
    Co-authored-by: github-actions <>

---
+++ src/test/java/com/mycompany/app/services/ShoppingCartServiceTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app.services;
+
+import org.junit.Test;
+
+public class ShoppingCartServiceTest {
+  @Test
+  public void testAddProduct() {}
+
+  @Test
+  public void testDeleteProduct() {}
+
+  @Test
+  public void testGetAllShoppingCart() {}
+}
